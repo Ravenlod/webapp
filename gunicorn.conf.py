db7303bd4f04a253106d95044c46c882bb9d09c5
Ravenlod <talgatomarov68@gmail.com>
2023-11-07 16:17:42 +0500

    minor fix

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,7 +1,7 @@
 workers = 1
 worker_class = "gevent"
 wsgi_app = "app:create_app()"
-bind = "0.0.0.0:5000"
+bind = "0.0.0.0:8081"
 # chdir = "/var/www/web"
 # errorlog = "/home/root/error.log"
 # accesslog = "/home/root/access.log"
